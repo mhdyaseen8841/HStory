e50fb3b97843f8821964233f6502a9a218ce194c
mohammed yaseen <yyaseen080@gmail.com>
2023-02-17 12:27:06 +0530

    working on selected chat opening

--- client/src/components/miscellaneous/ProfileModal.js
+++ client/src/components/miscellaneous/ProfileModal.js
@@ -1,11 +1,65 @@
 import React from 'react'
-import { useDisclosure } from '@chakra-ui/react'
+import { useDisclosure,Image,Text } from '@chakra-ui/react'
+import { IconButton } from '@chakra-ui/button'
+import { ViewIcon } from '@chakra-ui/icons'
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Button,
+} from '@chakra-ui/react'
 function ProfileModal({user, children}) {
 
   const { isOpen, onOpen, onClose } = useDisclosure()
   return (
     <>
-    <div>{children}</div>
+    {children ? ( <span onCLick={onOpen}>{children}</span> ):(
+ <IconButton d={{ base : "flex"}}
+ icon={<ViewIcon/>}
+onClick={onOpen}
+ />
+    )}
+
+<Modal size="lg" isCentered isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent h="410px">
+          <ModalHeader
+          fontSize="40px"
+          fontFamily="work sans"
+          display="flex"
+          justifyContent="center"
+          >{user.name}</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody display="flex"
+          flexDir="column"
+          alignItems="center"
+          justifyContent="space-between">
+          <Image borderRadius="full"
+          boxSize="150px"
+          src={user.pic}
+          alt={user.name}
+          />
+          <Text
+          fontSize={{base:"20px",md:"30px"}}
+fontFamily="work sans"
+>
+  Email: {user.email}
+</Text>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme='blue' mr={3} onClick={onClose}>
+              Close
+            </Button>
+            <Button variant='ghost'>Secondary Action</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+
     </>
   )
 }
--- client/src/components/miscellaneous/SideDrawer.js
+++ client/src/components/miscellaneous/SideDrawer.js
@@ -1,13 +1,87 @@
-import { Box, Tooltip, Button, Text, Menu, MenuButton, MenuList, MenuItem, MenuDivider } from "@chakra-ui/react";
+import { Box, Tooltip, Button, Text, Menu, MenuButton, MenuList, MenuItem, MenuDivider,Drawer, useDisclosure,Image, DrawerContent,DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerCloseButton,Input, useToast } from "@chakra-ui/react";
 import React, { useState } from "react";
 import {BellIcon,ChevronDownIcon} from '@chakra-ui/icons'
 import { Avatar, AvatarBadge, AvatarGroup } from '@chakra-ui/react'
 import { ChatState } from "../../Context/ChatProvider";
+import ProfileModal from "./ProfileModal";
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
+import ChatLoading from "./ChatLoading";
+import UserListItem from "../UserAvatar/UserListItem";
 function SideDrawer() {
+  const toast = useToast()
+  const { isOpen, onOpen, onClose } = useDisclosure()
   const [search, setSearch] = useState("");
   const [searchResults, setSearchResults] = useState([]);
   const [loading, setLoading] = useState(false);
   const [loadingChat, setLoadingChat] = useState(false);
+ 
+  const Navigate = useNavigate();
+
+
+const handleSearch = async() => {
+if(!search){
+  toast({
+    title: "Please Enter Something in search! ",
+    status: "warning",
+    duration: 5000,
+    isClosable: true,
+    position: "top-left"
+  })
+  return;
+}
+
+try{
+setLoading(true)
+
+const config = {
+  headers: {
+    Authorization: `Bearer ${user.token}`,
+  },
+}
+  const {data} = await axios.get(`/api/user?search=${search}`, config)
+
+  setLoading(false)
+  setSearchResults(data)
+}catch(error){
+  toast({
+    title: "Error Occured! ",
+    description: "Failed to search users! ",
+    status: "error",
+    duration: 5000,
+    isClosable: true,
+    position: "bottom-left"
+  })
+}
+  }
+
+const accessChat = async (userId) => {
+  try{
+setLoadingChat(true)
+
+
+const config = {
+  headers: {
+    "Content-type" : "application/json",
+    Authorization: `Bearer ${user.token}`,
+  },
+}
+ const {data} = await axios.post(`/api/chat`, {userId}, config)
+
+  }catch(err){
+
+  }
+
+}
+
+const logoutHandler = () => {
+  localStorage.removeItem("userInfo");
+  Navigate("/")
+}
 
   const {user} = ChatState();
   return (
@@ -22,7 +96,7 @@ function SideDrawer() {
         borderWidth="5px"
       >
         <Tooltip label="Search users to chat" hasArrow placement="bottom-end">
-          <Button variant="ghost">
+          <Button variant="ghost" onClick={onOpen}>
             <i class="fa-solid fa-magnifying-glass"></i>
             <Text display={{ sm: "none", md: "flex" }} px="4">
               Search User
@@ -45,14 +119,46 @@ function SideDrawer() {
 <Avatar size="sm" cursor="pointer" name={user.name} src={user.pic}/>
 </MenuButton>
 <MenuList>
-  <ProfielModal></ProfielModal>
-  <MenuItem>My Profile</MenuItem>
+  <ProfileModal user={user}>
+ 
+  </ProfileModal>
   <MenuDivider/>
-  <MenuItem>Logout</MenuItem>
+  <MenuItem onClick={logoutHandler}>Logout</MenuItem>
 </MenuList>
           </Menu>
         </div>
       </Box>
+
+
+      <Drawer placement="left" onClose={onClose} isOpen={isOpen} >
+        <DrawerOverlay />
+<DrawerContent>
+  <DrawerHeader borderBottomWidth="1px">Search Users</DrawerHeader>
+  <DrawerBody>
+<Box display="flex" pb={2}>
+  <Input placeholder="Search by name or email"
+  mr={2}
+  value={search}
+  onChange={(e)=> setSearch(e.target.value)}
+  />
+  <Button onClick={handleSearch}>Go</Button>
+</Box>
+{loading ? (
+<ChatLoading/>
+):(
+searchResults?.map(user =>(
+  <UserListItem
+  key={user._id}
+  user={user}
+  handleFunction={()=>accessChat(user._id)}
+  />
+))
+)
+}
+</DrawerBody>
+</DrawerContent>
+
+      </Drawer>
     </>
   );
 }
